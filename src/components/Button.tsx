ec35319c125497c2edc504bc31a692f4e3ffc518
Alexander Snov <snov.contact@gmail.com>
2018-07-11 03:50:23 +0700

    Add type prop

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,13 +3,18 @@ import React from 'react';
 import styles from './Button.module.css';
 
 export interface IButtonsProps {
+  type?: string;
   children?: any;
   onClick?: (e: React.MouseEvent<HTMLElement>) => void;
 }
 
-export const Button = ({ children, onClick }: IButtonsProps) => (
+export const Button = ({
+  children,
+  onClick,
+  type = 'button',
+}: IButtonsProps) => (
   <>
-    <button className={styles.button} type="submit" onClick={onClick}>
+    <button className={styles.button} type={type} onClick={onClick}>
       {children}
     </button>
   </>
