7962be90aa17b20563fa7f89fc4289db82279c37
Alexander Snov <snov.contact@gmail.com>
2018-07-11 03:50:05 +0700

    Use controlled input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -6,13 +6,27 @@ export interface IInputProps {
   placeholder: string;
   name: string;
   label: string;
+  value: string;
+  onChange: (a: any) => void;
 }
 
-export const Input = ({ placeholder, name, label }: IInputProps) => (
+export const Input = ({
+  placeholder,
+  name,
+  label,
+  value,
+  onChange,
+}: IInputProps) => (
   <>
     <label className={styles.label} htmlFor={name}>
       {label}
     </label>
-    <input className={styles.input} name={name} placeholder={placeholder} />
+    <input
+      className={styles.input}
+      name={name}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
   </>
 );
